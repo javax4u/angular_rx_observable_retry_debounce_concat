ed7d70bf33138e3eb234eaadeb8907f9453a6fea
rupesh kumar <javax4u@gmail.com>
2020-08-31 16:10:41 +0530

    git

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# rx_observable_retry_debounce_concat
+[For Full videos watch](https://www.youtube.com/watch?v=NwgZ4R7HJIw&list=PLLhsXdvz0qjI68a8tLUUMyXmNhl608mcn)
+
+
+![Browser Output](images/image_01.jpg)
+![Browser Output](images/image_02.jpg)
+![Browser Output](images/image_03.jpg)
---
+++ run.bat
@@ -0,0 +1,27 @@
+cls
+ng serve --host 0.0.0.0 --disable-host-check --ssl true
+#ng serve --host 0.0.0.0 --disable-host-check
+#ng serve
+#20200803
+##to generate a component
+#ng generate component camera
+#ng generate component fingerprint
+#ng generate component signaturepad
+#ng generate component employee-search
+#cmd.exe /c 'run.bat'
+#ng build --prod --base-href=angular
+#below command does not work at run time. many css is not found
+#ng build --prod
+#create war file for jboss
+#cd dist/angular-tour-of-heroes/
+#jar -cvf ../../angular.war *
+##firebase configurations
+#npm install -g firebase-tools
+#npm install --save @angular/fire@5.1.1 firebase@5.8.4
+#firebase login
+#firebase init
+##provide this path dist\angular-tour-of-heroes
+#ng build --prod
+#firebase deploy
+## change remote URL
+#git remote set-url origin https://github.com/javax4u/rxjs_observable_custom
