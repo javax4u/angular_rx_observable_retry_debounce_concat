19e8024f0a525532d35448606b125daa8ff48a77
rupesh kumar <javax4u@gmail.com>
2020-09-01 15:41:26 +0530

    debounce

--- src/app/observable/debounce/debounce.component.ts
+++ src/app/observable/debounce/debounce.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { map, distinctUntilChanged, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-debounce',
   templateUrl: './debounce.component.html',
   styleUrls: ['./debounce.component.css']
 })
-export class DebounceComponent implements OnInit {
+export class DebounceComponent  implements AfterViewInit {
+
+  @ViewChild('myInput') myInput:ElementRef;
+  @ViewChild('myInput2') myInput2:ElementRef;
+
+  requestData;
+  requestData2;
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+
+    // Ex 01
+    const searchTerm=fromEvent<any>(this.myInput.nativeElement,'keyup')
+    .pipe(map(event => event.target.value), debounceTime(500))
+    ;
+
+    searchTerm.subscribe(data => {
+      this.requestData=data;
+      console.log(data);
+    });
+
+    // Ex 02
+    const searchTerm2=fromEvent<any>(this.myInput2.nativeElement,'keyup')
+    .pipe(map(event => event.target.value),
+    debounceTime(500),
+    distinctUntilChanged())
+
+    searchTerm2.subscribe(data => {
+      this.requestData2=data;
+      console.log(data);
+
+      setTimeout(() => {
+        //this.requestData2=null;
+      },1000);
+    });
   }
 
 }
--- src/app/observable/retry/retry.component.ts
+++ src/app/observable/retry/retry.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DesignUtilityService } from 'src/app/appServices/design-utility.service';
+import { retry, retryWhen, delay, scan } from 'rxjs/operators';
 
 @Component({
   selector: 'app-retry',
@@ -9,17 +10,35 @@ import { DesignUtilityService } from 'src/app/appServices/design-utility.service
 })
 export class RetryComponent implements OnInit {
 
-  constructor(private httpClient:HttpClient,private designUtility:DesignUtilityService) { }
+  person;
+  fetchingStatus = -1; //0 means ready to fetching, 1 means fetch complete and 2 means error
+  statusMessage = ""; statusClass = "primary";
+  constructor(private httpClient: HttpClient, private designUtility: DesignUtilityService) { }
 
   ngOnInit() {
 
-    this.fetchDetails();
+    // this.fetchDetails();
   }
-  fetchDetails(){
+  fetchDetails() {
+    this.fetchingStatus = 0;
+    this.statusMessage = "Fetching. ...";
     //httpClient
     //get me file with this url
     this.httpClient.get('https://raw.githubusercontent.com/javax4u/rx_observable_retry_debounce_concat/master/employee.json')
-    .subscribe((data) => console.log(data));
+      .pipe(
+        retry(4)
+        //scan and delay code should come here. But rupesh was not able to comprehend it and there was error about missing ) and { on screen so removed it for a while
+        //https://www.youtube.com/watch?v=isFdiFdg8y0&list=PLLhsXdvz0qjI68a8tLUUMyXmNhl608mcn&index=19
+      )
+      .subscribe((data) => {
+        this.person = data;
+        console.log(data)
+        this.statusMessage = " Complete";
+        this.fetchingStatus = 1;
+      }, error => {
+        this.statusMessage = " Failed";
+        this.fetchingStatus = 2;
+      });
 
     //show file data on html
   }
